4f71b366a9d91261752fecc16e72b5cfd29e1a43
donggu1105 <kbh11051@gmail.com>
2022-06-21 14:21:17 +0900

    값 타입 의 비교

--- jpa-basic/src/main/java/jpabook/jpashop/domain/Address.java
+++ jpa-basic/src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,15 +1,24 @@
 package jpabook.jpashop.domain;
 
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
 public class Address {
 
-
-
     private String city;
     private String street;
     private String zipcode;
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+
+    public Address() {
+
+    }
 
     public void setCity(String city) {
         this.city = city;
@@ -33,5 +42,18 @@ public class Address {
 
     public String getZipcode() {
         return zipcode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(getCity(), address.getCity()) && Objects.equals(getStreet(), address.getStreet()) && Objects.equals(getZipcode(), address.getZipcode());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCity(), getStreet(), getZipcode());
     }
 }
