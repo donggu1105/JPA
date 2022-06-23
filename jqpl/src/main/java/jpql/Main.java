e202fea4eb1731c344098d3ee3a2ffec7605d03d
donggu1105 <kbh11051@gmail.com>
2022-06-23 15:00:23 +0900

    case

--- jqpl/src/main/java/jpql/Main.java
+++ jqpl/src/main/java/jpql/Main.java
@@ -19,33 +19,29 @@ public class Main {
         tx.begin();
         try {
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+//            Team team = new Team();
+//            team.setName("teamA");
+//            em.persist(team);
 
-            Member member = new Member();
-            member.setAge(10);
-            member.setUsername("test");
-            member.setTeam(team);
-            em.persist(member);
+            for (int i = 0; i < 100; i++) {
+
+                Member member = new Member();
+                member.setAge(i);
+                member.setUsername(null);
+    //            member.setTeam(team);
+                em.persist(member);
+            }
 
             em.flush();
             em.clear();
 
-//            List<Member> members = em.createQuery("select m from Member m order by m.age desc")
-//                    .setFirstResult(1)
-//                    .setMaxResults(10)
-//                    .getResultList();
-            String query = "select m, t from Member m left join m.team t on t.name  = 'teamA'";
-            List<Member> result = em.createQuery(query, Member.class)
-//                    .setParameter("teamName", "teamA")
-//                    .setFirstResult(1)
-//                    .setMaxResults(10)
-                    .getResultList();
+            String query = "select coalesce(m.username, '이름없는 회원') from Member m";
+            List<String> members = em.createQuery(query).getResultList();
+            for (String m : members) {
+                System.out.println(m);
+            }
 
-//            for (Member m : members) {
-//                System.out.println(m);
-//            }
+
 
             tx.commit();
         } catch (Exception e) {
