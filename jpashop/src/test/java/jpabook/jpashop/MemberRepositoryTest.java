4d9bc31e85f033b75bf0369e96c23388ded1a12a
donggu1105 <kbh11051@gmail.com>
2022-06-24 13:52:50 +0900

    jpa

---
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,50 @@
+package jpabook.jpashop;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+    
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember() throws Exception {
+        // given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        // when
+        Long savedId = memberRepository.save(member);
+        // then
+        Member findMember = memberRepository.find(savedId);
+
+        Assertions.assertEquals(findMember.getId(), member.getId());
+        Assertions.assertEquals(findMember.getUsername(), member.getUsername());
+        Assertions.assertEquals(findMember, member );
+
+    }
+    
+    @Test
+    void save() {
+
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    void find() {
+    }
+}
