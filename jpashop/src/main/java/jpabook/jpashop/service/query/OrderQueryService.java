13277408104f409f66230cc101500fef91ab96eb
donggu1105 <kbh11051@gmail.com>
2022-07-02 19:00:10 +0900

    123

---
+++ jpashop/src/main/java/jpabook/jpashop/service/query/OrderQueryService.java
@@ -0,0 +1,11 @@
+package jpabook.jpashop.service.query;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+@Service
+@RequiredArgsConstructor
+public class OrderQueryService  {
+}
